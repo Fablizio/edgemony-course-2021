7b2cd60d5acbda8102f496887ea7546463e02fe4
Fablizio <gats200@hotmail.it>
2021-10-25 16:07:33 +0200

    esercizio completo

--- 25Oct/esercitazione.js
+++ 25Oct/esercitazione.js
@@ -5,7 +5,7 @@
 */
 
 // Es. 1: Dato il seguente 'snippet' (piccolo programmino), qual'è il risultato dei console.log?
-// RISPOSTA:
+// RISPOSTA: Stampa PI per due volte il primo del consolle log riga 11 successivamente viene stampato quello dentro if essendo maggiore di 3
 
 const PI = 3.14;
 
@@ -16,7 +16,7 @@ if (PI >= 3) {
 }
 
 // Es. 2: Dato il seguente, qual'è il risultato dei console.log?
-// RISPOSTA:
+// RISPOSTA: Il mio colore preferito è violet e subito sotto Ci sono 12 mesi in un anno (il primo viene stampato perchè la condizione true è verificata successivamente il secondo esterno dalla condizione)
 
 var favColour = "violet";
 let monthsInAYear = 12;
@@ -29,7 +29,8 @@ if (true) {
 console.log("Ci sono", monthsInAYear, "mesi in un anno.");
 
 // Es. 3: Dato il seguente, qual'è il risultato dei console.log?
-// RISPOSTA:
+// RISPOSTA: (questo viene gestita dalla prima funcion che richiama l'if) Sugo di pomodoro freschissimo, rucola, prosciutto crudo e scaglie di grana.
+//dalla linea 42 alla 45 non viene dichiaratata la variabile theSecond che quindi richiamerà un errore Uncaught ReferenceError
 
 function makePizza(moreIngredients) {
   let theSecond = "Sugo di pomodoro freschissimo";
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # edgemony-course-2021
 
 Repository lezioni cooding bootcamp 2021/22
+
+Questo è l'esercizio completo del 25/10/2021
