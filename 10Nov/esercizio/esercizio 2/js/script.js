f7f993aa3d7d1e1284f2562fb00c6a6becb9d88a
Fablizio <gats200@hotmail.it>
2021-11-10 17:48:35 +0100

    esercizio 1 completo, il secondo in lavorazione

---
+++ 10Nov/esercizio/esercizio 2/js/script.js
@@ -0,0 +1,85 @@
+const sum = (numOne, numTwo) => numOne + numTwo;
+const mult = (numOne, numTwo) => numOne * numTwo;
+const sottr = (numOne, numTwo) => numOne - numTwo;
+const divis = (numOne, numTwo) => numOne / numTwo;
+const potens = (numOne, numTwo) => Math.pow(numOne, numTwo);
+const radic = (numOne) => Math.sqrt(numOne);
+
+function operationFunc(operation, parsedNumbers) {
+
+    if (operation === "somma" || '+') {
+        return sum(parsedNumbers);
+    }
+
+    if (operation === "moltiplicazione" || '*') {
+        return mult(parsedNumbers);
+    }
+
+    if (operation === "sottrazione" || '-') {
+        return sottr(parsedNumbers);
+    }
+
+    if (operation === "divisione" || '/') {
+        return divis(parsedNumbers);
+    }
+
+    if (operation === "potenza") {
+        return potens(parsedNumbers);
+    }
+
+    if (operation === "radice" || '√') {
+        return radic(parsedNumbers);
+    } else {
+        allert('Attenzione Bisogna inserire almeno un numero');
+    }
+}
+
+
+function calculator(numbers) {
+    let totOperation = 0;
+    totOperation = operationFunc(...numbers);
+    return 'Il risultato è: ', totOperation;
+}
+
+
+let operation = prompt("Inserisci operazione: ");
+let numbersChoiche = prompt("Inserisci numeri: ");
+try {
+    switch (errori) {
+        case soma:
+            operation !== "+"; throw "Devi inserire una operazione";
+    }
+    // if (operation !== "+") throw "Devi inserire una operazione";
+    // if (operation !== "somma") throw "Devi inserire una operazione";
+    // if (operation !== "-") throw "Devi inserire una operazione";
+    // if (operation !== "sottrazione") throw "Devi inserire una operazione ";
+    // if (operation !== "*") throw "Devi inserire una operazione";
+    // if (operation !== "moltiplicazione") throw "Devi inserire una operazione";
+    // if (operation !== "/") throw "Devi inserire una operazione";
+    // if (operation !== "divisione") throw "Devi inserire una operazione ";
+    // if (operation !== "potenza") throw "Devi inserire una operazione ";
+    // if (operation !== "radice") throw "Devi inserire una operazione ";
+    // if (operation !== "√") throw "Devi inserire una operazione ";
+    // if (operation === "") throw "Devi inserire una operazione";
+    // if (numbersChoiche === "") throw "Devi inserire almeno un numero";
+    // if (numbersChoiche === "") throw "Devi inserire almeno un numero";
+    // if (numbersChoiche !== "\,") throw "Devi inserire almeno un numero";
+} catch (err) {
+    console.log(err);
+} finally {
+    console.log("Bravo.");
+}
+const numbers = [...numbersChoiche.split(",")];
+console.log(numbers)
+const parsedNumbers = [];
+
+for (x of numbers) {
+    parsedNumbers.push(parseInt(x));
+}
+
+
+
+
+calculator(operation, parsedNumbers);
+
+console.log(calculator(operation, parsedNumbers));
---
+++ 10Nov/esercizio/js/script.js
@@ -0,0 +1,44 @@
+
+
+function sendCredentials(username) {
+    nomeC.textContent = `${username}`;
+    // console.log(userPage.firstElementChild.textContent);
+    // userPage.firstElementChild.textContent.replace("NOME", username);
+}
+
+function saveCredentials(user, pass) {
+    window.localStorage.setItem("username", user);
+    window.localStorage.setItem("password", pass);
+}
+
+function loadCredentials(key) {
+    return window.localStorage.getItem(key);
+}
+
+const userPage = document.querySelector(".userPage");
+const modalEl = document.querySelector(".modal");
+const userInputEl = document.querySelector("#user");
+const passInputEl = document.querySelector("#pass");
+const btnSendEl = document.querySelector("#btnSend");
+const nomeC = document.querySelector("#nome");
+
+const user = {
+    username: "",
+    password: "",
+};
+
+
+
+// LOCAL STORAGE ----->
+sendCredentials(loadCredentials("username"));
+
+btnSendEl.addEventListener("click", (evt) => {
+    user.username = userInputEl.value;
+    user.password = passInputEl.value;
+
+    saveCredentials(user.username, user.password);
+
+    const userStorage = window.localStorage.getItem("username");
+
+    sendCredentials(userStorage);
+});
